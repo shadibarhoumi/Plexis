b2c899cc3f85aebcf3b614c7425b9ad227bfb6c3
Shadi Barhoumi <shadibarhoumi@gmail.com>
2013-06-04 05:49:03 -0700

    branching working

--- plexis.js
+++ plexis.js
@@ -3,20 +3,75 @@
 
 if (Meteor.isClient) {
 
-  Template.chat.chat = function() {
-    return Messages.find({}).fetch();
+  Template.conversation.branches = function() {
+    var messages = Messages.find({}, {sort: [["branchId", "asc"], ["timestamp", "asc"]]}).fetch();
+    var branches = []
+    var currentBranch = -1;
+    for (var i = 0; i < messages.length; i++) {
+      if (currentBranch !== messages[i].branchId) {
+        console.log('creating new branch!');
+        branch = {branchId: messages[i].branchId, messages: [messages[i]]};
+        branches.push(branch);
+        currentBranch = messages[i].branchId
+      } else {
+        console.log('pushing to existing branch with message: ' + messages[i].message);
+        branch.messages.push(messages[i]);
+      }
+    }
+    return branches;
   };
 
-  Template.chat.events({
-    'click #submit, keyup #message': function(e) {
-      if (e.keyCode === 13 || e.type === 'click') {
-          message = $('#message').val()
-          Messages.insert({message: message, owner: Meteor.userId(), username: Meteor.user().emails[0].address.replace(/\@.*$/, '')});
-          $('#message').val('');
+  Template.conversation.events({
+    'keyup .message': function(e) {
+      if (e.keyCode === 13) {
+        if (Meteor.user()) {
+          console.log('putting stuff in database in keyup .message');
+            Messages.insert({message: $(e.target).val(),
+                            parentId: $(e.target).prev()[0].id,
+                            branchId: $(e.target).parent().data('branchid'),
+                            owner: Meteor.userId(),
+                            username: Meteor.user().emails[0].address.replace(/\@.*$/, ''),
+                            timestamp: new Date});
+            $('.message').val('');
+        } else {
+          alert('you must be logged in to do this!');
+        }
       }
-      return false;
-    }
-  });
+        return false;
+      },
+
+      'click .submit': function(e) {
+        if (Meteor.user()) {
+          var $message = $(e.target).siblings('.message')
+          console.log('putting stuff in database in click .submit');
+            Messages.insert({message: $message.val(),
+                            parentId: $message.prev()[0].id,
+                            branchId: $(e.target).parent().data('branchid'),
+                            owner: Meteor.userId(),
+                            username: Meteor.user().emails[0].address.replace(/\@.*$/, ''),
+                            timestamp: new Date});
+            $message.val('');
+        } else {
+          alert('you must be logged in to do this!')
+        }
+        return false;
+      },
+
+      'click .branch-link': function(e) {
+        // optimize this so we don't have to do another lookup
+        var messages = Messages.find({}, {sort: [["branchId", "desc"]]}).fetch();
+        var nextBranch = parseInt(messages[0].branchId) + 1;
+
+        Messages.insert({
+          message: $(e.target).siblings('.message-text').text(),
+          parentId: $(e.target).parent()[0].id,
+          branchId: nextBranch,
+          owner: Meteor.userId(),
+          username: Meteor.user().emails[0].address.replace(/\@.*$/, ''),
+          timestamp: new Date});
+        return false;
+      }
+    });
 
 }
 
