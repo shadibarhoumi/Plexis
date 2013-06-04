4f651c2bf89dd9c5b6202edbc4b5c05ca6d2f8ba
Shadi Barhoumi <shadibarhoumi@gmail.com>
2013-06-03 21:53:26 -0700

    removed extra plexis folder

--- model.js
+++ model.js
@@ -2,7 +2,6 @@ Messages = new Meteor.Collection("messages");
 
 Messages.allow({
 	insert: function(userId, message) {
-		console.log('in message.allow');
 		return userId && message.owner === userId;
 	}
 });
